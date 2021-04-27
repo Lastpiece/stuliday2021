7ed4bffff1faac923f6dc1f53f20a14574980a17
Hamza <hamza.lakhdari@viacesi.fr>
2021-04-27 19:11:56 +0200

    refound for bulma

--- docNav.php
+++ docNav.php
@@ -10,7 +10,7 @@
         <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> -->
 
         <!-- dart-sass/sass scss/style.scss css/style.css --watch -->
-        <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css"> -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
         <!-- <link rel="stylesheet" href="assets/css/lib/bootstrap.min.css"> -->
         <link rel="stylesheet" href="assets/css/style.css">
 
