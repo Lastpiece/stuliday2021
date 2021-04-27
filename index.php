46aabbb1957eb1a53f9dd5b1dbf09365e63c2f45
Hamza <hamza.lakhdari@viacesi.fr>
2021-04-27 18:57:35 +0200

    adding all page includ navbar

--- index.php
+++ index.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    require "inc/config.php";
+    include('docNav.php');
+?>
 
-    <head>
-
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3">
-
-        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> -->
-
-        <!-- dart-sass/sass scss/style.scss css/style.css --watch -->
-        <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css"> -->
-        <!-- <link rel="stylesheet" href="assets/css/lib/bootstrap.min.css"> -->
-        <link rel="stylesheet" href="assets/css/style.css">
-
-        <title>Stuliday</title>
-
-    </head>
-
-    <body>
-            
         <main>
             <h1>Stuliday</h1>
             <p>Vos location saisonnière entre particuliers</p>
@@ -27,6 +10,7 @@
                 <a href="#">Learn more</a>
             </p>
         </main>
+
 
         <footer>
             <p>Created<a href="" ></a>, by <a href="https://twitter.com/" >@lastpiece</a>.</p>
