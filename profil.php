bd05b362bb26d530ad90e774c5a68c845e8e7095
Hamza <hamza.lakhdari@viacesi.fr>
2021-04-29 17:16:01 +0200

    debut profil

--- profil.php
+++ profil.php
@@ -1,3 +1,16 @@
 <?php require "inc/header.php"; ?>
+    <div class="columns">
+        <div class="column">
 
+        </div>
+        <div class="column">
+
+        </div>
+        <div class="column">
+
+        </div>
+        <div class="column">
+
+        </div>
+    </div>
 <?php require "inc/footer.php"; ?>
